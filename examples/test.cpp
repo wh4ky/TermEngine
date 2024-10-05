a1590a7152376feb6b0e5e1274cb80b50ba91a69
wh4ky <damian.whacky@proton.me>
2024-10-05 21:20:36 +0200

    formatted

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,140 +1,120 @@
 // TEST do not use
 
 #include <iostream>
-#include <unistd.h>
-#include <termios.h>
 #include <signal.h>
 #include <string>
+#include <termios.h>
 #include <tuple>
+#include <unistd.h>
 
-namespace TE
-{
-    class IO
-    {
-    private:
-        static struct termios original;
+namespace TE {
+class IO {
+private:
+  static struct termios original;
 
-        static void signalHandle(int signum)
-        {
-        revertTerminal();
-        disableMouseReporting();
-        exit(0);
-        }
+  static void signalHandle(int signum) {
+    revertTerminal();
+    disableMouseReporting();
+    exit(0);
+  }
 
-        static void disableMouseReporting()
-        {
-            std::cout << "\033[?1003l";
-            std::cout.flush();
-            std::cout << "\033[?1006l";
-            std::cout.flush();
-        }
+  static void disableMouseReporting() {
+    std::cout << "\033[?1003l";
+    std::cout.flush();
+    std::cout << "\033[?1006l";
+    std::cout.flush();
+  }
 
-        static void enableMouseReporting()
-        {
-            std::cout << "\033[?1003h";
-            std::cout.flush();
-            std::cout << "\033[?1006h";
-            std::cout.flush();
-        }
+  static void enableMouseReporting() {
+    std::cout << "\033[?1003h";
+    std::cout.flush();
+    std::cout << "\033[?1006h";
+    std::cout.flush();
+  }
 
-        static void revertTerminal()
-        {
-            tcsetattr(STDIN_FILENO, TCSANOW, &original);
-        }
+  static void revertTerminal() { tcsetattr(STDIN_FILENO, TCSANOW, &original); }
 
-    public:
-        IO()
-        {
-            signal(SIGINT, signalHandle);
+public:
+  IO() {
+    signal(SIGINT, signalHandle);
 
-            std::cout << "\033[?1003h";
-            std::cout.flush();
-            std::cout << "\033[?1006h";
-            std::cout.flush();
+    std::cout << "\033[?1003h";
+    std::cout.flush();
+    std::cout << "\033[?1006h";
+    std::cout.flush();
 
-            tcgetattr(STDIN_FILENO, &original);
-            struct termios raw = original;
-            raw.c_lflag &= ~(ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-        }
+    tcgetattr(STDIN_FILENO, &original);
+    struct termios raw = original;
+    raw.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  }
 
-        ~IO()
-        {
-            revertTerminal();
-            disableMouseReporting();
-        }
+  ~IO() {
+    revertTerminal();
+    disableMouseReporting();
+  }
 
-        void revertSettings()
-        {
-            revertTerminal();
-            disableMouseReporting();
-        }
+  void revertSettings() {
+    revertTerminal();
+    disableMouseReporting();
+  }
 
-        std::string getKey() // function to get single keys (To Do: F-row and other keys)
-        {
-            disableMouseReporting();
-            char buf[10];
-            int bytesRead = read(STDIN_FILENO, buf, sizeof(buf) - 1);
-            buf[bytesRead] = '\0';
+  std::string
+  getKey() // function to get single keys (To Do: F-row and other keys)
+  {
+    disableMouseReporting();
+    char buf[10];
+    int bytesRead = read(STDIN_FILENO, buf, sizeof(buf) - 1);
+    buf[bytesRead] = '\0';
 
-            std::string str(buf);
+    std::string str(buf);
 
-            std::cout << str << " control" << std::endl;
-            
-            std::string out;
-            
-            
+    std::cout << str << " control" << std::endl;
 
-            return out;
-        }
-    };
+    std::string out;
 
-    /*  Function to get mouse clicks
-    std::tuple getMouse()
-        {
-            char buf[32];
-            int bytesRead = read(STDIN_FILENO, buf, sizeof(buf) - 1);
+    return out;
+  }
+};
 
-            if (bytesRead <= 1)
-            {
-                buf[bytesRead] = '\0';
+/* std::tuple getMouse() { Function to get mouse clicks
+  char buf[32];
+  int bytesRead = read(STDIN_FILENO, buf, sizeof(buf) - 1);
 
-                for (int i = 0; i < bytesRead; ++i) {
-                    printf("%c ", buf[i]);
-                }
-                std::cout.flush(); 
+  if (bytesRead <= 1) {
+    buf[bytesRead] = '\0';
 
-                std::string str(buf);
-                return str;
-            }
-            return "null";
-        }
-    };
-    */
+    for (int i = 0; i < bytesRead; ++i) {
+      printf("%c ", buf[i]);
+    }
+    std::cout.flush();
 
-    struct termios IO::original;
+    std::string str(buf);
+    return str;
+  }
+  return "null";
 }
+};*/
 
-int main()
-{
-    TE::IO io;
+struct termios IO::original;
+} // namespace TE
 
-    while (true)
-    {
-        if (io.getKey() == "q")
-        {
-            io.revertSettings();
-            return 0;
-        }
-        
-        else if (io.getKey() != "null")
-        {
-            std::cout << io.getKey() << std::endl;
-        }
+int main() {
+  TE::IO io;
 
-        usleep(1000);
+  while (true) {
+    if (io.getKey() == "q") {
+      io.revertSettings();
+      return 0;
     }
 
-    io.revertSettings();
-    return 0;
+    else if (io.getKey() != "null") {
+      std::cout << io.getKey() << std::endl;
+    }
+
+    usleep(1000);
+  }
+
+  io.revertSettings();
+  return 0;
 }
